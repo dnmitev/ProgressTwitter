afb4fd43893d418182efedf9c21abf16e5f6b849
Dimitar Mitev <dnmitev@gmail.com>
2016-07-23 14:59:40 +0300

    MongoDB repository created

---
+++ Database/ProgressTwitter.Contracts/IEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace ProgressTwitter.Contracts
+{
+    using MongoDB.Bson.Serialization.Attributes;
+
+    /// <summary>
+    /// General interface for a DB entity
+    /// </summary>
+    /// <typeparam name="T">The type of the Id</typeparam>
+    public interface IEntity<T>
+    {
+        [BsonId]
+        T Id { get; set; }
+    }
+}
