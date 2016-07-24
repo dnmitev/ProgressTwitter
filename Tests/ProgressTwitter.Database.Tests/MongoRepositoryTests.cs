e48803d04d23c6fba7f102b3da1524ad62ef8a6d
Dimitar Mitev <dnmitev@gmail.com>
2016-07-24 18:29:30 +0300

    added scaffold for the unit tests solution; removed redundant files from the DB projects

---
+++ Tests/ProgressTwitter.Database.Tests/MongoRepositoryTests.cs
@@ -0,0 +1,37 @@
+﻿namespace ProgressTwitter.Database.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using MongoDB.Driver;
+    using System.Configuration;
+
+    [TestClass]
+    public class MongoRepositoryTests
+    {
+        [TestInitialize]
+        public void Setup()
+        {
+            // to be sure that each time the DB is created by the repository
+            // no old DB  left
+            this.DropDB();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            this.DropDB();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        private void DropDB()
+        {
+            var url = new MongoUrl(ConfigurationManager.ConnectionStrings["MongoDefaultConnection"].ConnectionString);
+            var client = new MongoClient(url);
+            client.DropDatabase(url.DatabaseName);
+        }
+    }
+}
