e19bf2dcf525a15578c6aa7531fa10b508272555
Dimitar Mitev <dnmitev@gmail.com>
2016-07-30 19:28:50 +0300

    added unit test on the mongo repo and added web project

---
+++ ProgressTwitter.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProgressTwitter.Web.Startup))]
+namespace ProgressTwitter.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
--- Tests/ProgressTwitter.Database.Tests/MongoRepositoryTests.cs
+++ Tests/ProgressTwitter.Database.Tests/MongoRepositoryTests.cs
@@ -1,13 +1,11 @@
 ﻿namespace ProgressTwitter.Database.Tests
 {
-    using System;
+    using Contracts;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using MongoDB.Driver;
-    using System.Configuration;
     using Repositories;
-    using Entities.Base;
-    using Contracts;
-    using Attributes;
+    using System.Configuration;
+    using System.Linq;
 
     [TestClass]
     public class MongoRepositoryTests
@@ -37,17 +35,185 @@
             Assert.AreEqual(repo.Collection.CollectionNamespace.CollectionName, "Products");
         }
 
+        [TestMethod]
+        public void ShoudlAddOneItemToCollection()
+        {
+            repo = new MongoRepository<Product>();
+            repo.Add(new Product() { Price = 10 });
+
+            Assert.AreEqual(1, repo.GetAll().Count());
+        }
+
+        [TestMethod]
+        public void ShoudlGenerateIdAutomaticallyOnAdd()
+        {
+            repo = new MongoRepository<Product>();
+            repo.Add(new Product() { Price = 10 });
+
+            Assert.IsInstanceOfType(repo.GetAll().FirstOrDefault().Id, typeof(string));
+        }
+
+
+        [TestMethod]
+        public void ShoudlAddManyItems()
+        {
+            repo = new MongoRepository<Product>();
+            for (int i = 0; i < 10; i++)
+            {
+                repo.Add(new Product() { Price = i });
+            }
+
+            Assert.AreEqual(10, repo.GetAll().Count());
+        }
+
+        [TestMethod]
+        public void ShoudGetItemById()
+        {
+            repo = new MongoRepository<Product>();
+            repo.Add(new Product() { Price = 10 });
+
+            var item = repo.GetAll().FirstOrDefault();
+            var actual = repo.GetById(item.Id);
+
+            Assert.AreEqual(item.Id, actual.Id);
+        }
+
+        [TestMethod]
+        public void ShoudDeleteItemById()
+        {
+            repo = new MongoRepository<Product>();
+            repo.Add(new Product() { Price = 10 });
+
+            var item = repo.GetAll().FirstOrDefault();
+            repo.Delete(item.Id);
+
+            Assert.AreEqual(0, repo.GetAll().Count());
+        }
+
+        [TestMethod]
+        public void ShoudDeleteItem()
+        {
+            repo = new MongoRepository<Product>();
+            repo.Add(new Product() { Price = 10 });
+
+            var item = repo.GetAll().FirstOrDefault();
+            repo.Delete(item);
+
+            Assert.AreEqual(0, repo.GetAll().Count());
+        }
+
+        [TestMethod]
+        public void ShoudlDeleteAll()
+        {
+            repo = new MongoRepository<Product>();
+            for (int i = 0; i < 10; i++)
+            {
+                repo.Add(new Product() { Price = i });
+            }
+
+            repo.DeleteAll();
+
+            Assert.AreEqual(0, repo.GetAll().Count());
+        }
+
+        [TestMethod]
+        public void ShouldDeleteByCondition()
+        {
+            repo = new MongoRepository<Product>();
+            for (int i = 0; i < 10; i++)
+            {
+                repo.Add(new Product() { Price = i });
+            }
+
+            repo.Delete(p => p.Price > 7);
+
+            Assert.AreEqual(8, repo.Count());
+        }
+
+        [TestMethod]
+        public void ShoudlUpdateMultipleItem()
+        {
+            repo = new MongoRepository<Product>();
+            for (int i = 0; i < 10; i++)
+            {
+                repo.Add(new Product() { Price = i });
+            }
+
+            var items = repo.GetAll();
+            foreach (var item in items)
+            {
+                item.Price += 1000;
+            }
+
+            repo.Update(items);
+
+            var actual = repo.GetAll();
+
+            var isUpdated = true;
+            foreach (var item in actual)
+            {
+                isUpdated = item.Price < 1000 ? false : true;
+            }
+
+            Assert.IsTrue(isUpdated);
+        }
+
+
+        [TestMethod]
+        public void ShoudlUpdateItem()
+        {
+            repo = new MongoRepository<Product>();
+            repo.Add(new Product() { Price = 10 });
+
+            var item = repo.GetAll().FirstOrDefault();
+            item.Price = 102;
+
+            repo.Update(item);
+
+            Assert.AreEqual(item.Price, repo.GetById(item.Id).Price);
+        }
+
+        [TestMethod]
+        public void ShoudlHaveCount()
+        {
+            repo = new MongoRepository<Product>();
+            for (int i = 0; i < 10; i++)
+            {
+                repo.Add(new Product() { Price = i });
+            }
+
+            Assert.AreEqual(10, repo.Count());
+        }
+
+        [TestMethod]
+        public void ShouldReturnTrueIfExists()
+        {
+            repo = new MongoRepository<Product>();
+            for (int i = 0; i < 10; i++)
+            {
+                repo.Add(new Product() { Price = i });
+            }
+
+            Assert.IsTrue(repo.Exists(p => p.Price > 5));
+        }
+
+        [TestMethod]
+        public void ShouldReturnFalseIfDoesNoExist()
+        {
+            repo = new MongoRepository<Product>();
+            for (int i = 0; i < 10; i++)
+            {
+                repo.Add(new Product() { Price = i });
+            }
+
+            Assert.IsTrue(repo.Exists(p => p.Price > 10));
+        }
+
         private void DropDB()
         {
             var url = new MongoUrl(ConfigurationManager.ConnectionStrings["MongoDefaultConnection"].ConnectionString);
             var client = new MongoClient(url);
             client.DropDatabase(url.DatabaseName);
         }
-    }
-
-    [CollectionName("Products")]
-    public class Product : Entity
-    {
-        public decimal Price { get; set; }
     }
 }
